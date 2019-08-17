c23ada958d762b359063749b46320a83e3a51147
tsubasa1122 <nakanot@open8.com>
2019-08-17 22:16:03 +0900

    コールバックの実装

--- app/models/task.rb
+++ app/models/task.rb
@@ -3,9 +3,15 @@ class Task < ApplicationRecord
   validates :name, length: { maxinum: 30 }
   validate :validate_name_not_including_comma
 
+  before_validation :set_nameless_name
+
   private
 
   def validate_name_not_including_comma
     errors.add(:name, 'にカンマを含めることは出来ません。') if name&.include?(',')
+  end
+
+  def set_nameless_name
+    self.name = '名前なし' if name.blank?
   end
 end
