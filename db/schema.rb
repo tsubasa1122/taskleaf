4d5a4a9e4f665fd99d2f02a6033150be3a8d667a
tsubasa1122 <nakanot@open8.com>
2019-08-17 22:00:20 +0900

    nameカラムをnull: falseに

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,10 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_08_15_131755) do
+ActiveRecord::Schema.define(version: 2019_08_17_124914) do
 
   create_table "tasks", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
-    t.string "name"
+    t.string "name", null: false
     t.text "description"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
