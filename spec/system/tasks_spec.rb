41f0005af65b6a433798912e0863642268df9556
tsubasa1122 <nakanot@open8.com>
2019-08-31 22:17:39 +0900

    テスト修正

--- spec/system/tasks_spec.rb
+++ spec/system/tasks_spec.rb
@@ -5,14 +5,14 @@ describe 'タスク管理機能', type: :system do
     before do
       user_a = FactoryBot.create(:user, name: 'ユーザーA', email: 'a@example.com')
       FactoryBot.create(:task, name: '最初のタスク', user: user_a)
-    end
-    context 'ユーザーAがログインしているとき' do
       before do
         visit login_path
         fill_in 'メールアドレス', with: 'a@example.com'
         fill_in 'パスワード', with: 'password'
         click_button 'ログインする'
       end
+    end
+    context 'ユーザーAがログインしているとき' do
       it 'ユーザーAが作成したタスクが表示される' do
         expect(page).to have_content '最初のタスク'
       end
